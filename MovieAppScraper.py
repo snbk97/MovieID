97ea4304f36705846850eb4e4fed8d54646eb943
snbk97 <snbk97@gmail.com>
2018-03-30 12:14:41 +0530

    Initial Commit

---
+++ MovieAppScraper.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+
+URL = "https://www.imdb.com/chart/boxoffice"
+data = requests.get(URL).text
+
+soup = BeautifulSoup(data, 'html.parser')
+
+cols = soup.find_all('td', attrs={'class': 'titleColumn'})
+
+with open('MovieID', 'w') as file:
+    for col in cols:
+        current_col = ('tt' + col.find('a').get('href').split('?')
+                       [0].strip('/title/'))
+        file.write(current_col + '\n')
