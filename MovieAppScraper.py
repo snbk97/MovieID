2b9303e449e0862feedd366533f593c431323df1
snbk97 <snbk97@gmail.com>
2018-03-30 13:11:43 +0530

    represnt list as CSV

--- MovieAppScraper.py
+++ MovieAppScraper.py
@@ -12,4 +12,4 @@ with open('MovieID', 'w') as file:
     for col in cols:
         current_col = ('tt' + col.find('a').get('href').split('?')
                        [0].strip('/title/'))
-        file.write(current_col + '\n')
+        file.write(current_col + ',')
